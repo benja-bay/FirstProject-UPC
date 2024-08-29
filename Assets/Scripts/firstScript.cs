784660985f297f31678ff1e600847542b9892a50
benja-bay <benjaminbayarri17@gmail.com>
2024-08-29 19:33:40 -0300

    Clase 29/08

--- Assets/Scripts/firstScript.cs
+++ Assets/Scripts/firstScript.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class firstScript : MonoBehaviour
 {
+    string name = "Benjamin Bayarri";
+    int age = 21 ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(message:name +" "+ age);
     }
-
     // Update is called once per frame
     void Update()
     {
